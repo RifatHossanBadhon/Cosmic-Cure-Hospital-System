9dc5194d6aa0e1e1c0e9aa0c58fbe6ba10122645
Rifat Hossan Badhon <161221951+RifatHossanBadhon@users.noreply.github.com>
2025-08-09 06:25:59 +0600

    Add files via upload

---
+++ cosmiccure/src/main/java/com/cosmiccure/backend/AppointmentRequest.java
@@ -0,0 +1,12 @@
+package com.cosmiccure.backend;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class AppointmentRequest {
+    private Long patientId;
+    private Long doctorId;
+    private LocalDateTime appointmentTime;
+}
