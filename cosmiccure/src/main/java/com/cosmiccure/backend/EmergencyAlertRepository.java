895d2fd9785165ad79c0fb9c381f20997ba2fe3c
Rifat Hossan Badhon <161221951+RifatHossanBadhon@users.noreply.github.com>
2025-08-09 22:54:51 +0600

    Delete cosmiccure directory

--- cosmiccure/src/main/java/com/cosmiccure/backend/EmergencyAlertRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cosmiccure.backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmergencyAlertRepository extends JpaRepository<EmergencyAlert, Long> {
-}
